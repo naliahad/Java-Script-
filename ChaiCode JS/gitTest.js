9781f51c5f3d8e062ca6f6d8240b9a53d4d486f2
naliahad <ahadali2842000@gmail.com>
2024-07-21 03:46:52 +0530

    Git initialization

--- ChaiCode JS/gitTest.js
+++ ChaiCode JS/gitTest.js
@@ -14,3 +14,9 @@ git branch -M main
 git remote add origin https://github.com/naliahad/Java-Script-.git
 git push -u origin main
 */
+
+/* or push an existing repository from the command line
+git remote add origin https://github.com/naliahad/Java-Script-.git
+git branch -M main
+git push -u origin main
+*/
