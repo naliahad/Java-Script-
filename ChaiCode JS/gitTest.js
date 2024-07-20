ce1c0688afddf4f05379a88bd6b859ed745aec13
naliahad <ahadali2842000@gmail.com>
2024-07-21 02:06:41 +0530

    git adding details

---
+++ ChaiCode JS/gitTest.js
@@ -0,0 +1,16 @@
+/* ls -a
+git remote -v
+git remote rm origin
+git remote -v
+sign in github account ->  Create Repo -> public -> create
+git status
+git config user.name naliahad
+git config user.email ahadali2842000@gmail.com
+git add .
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/naliahad/Java-Script-.git
+git push -u origin main
+*/
